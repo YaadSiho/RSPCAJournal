596c98163c26dc597409daf4c9895d763ac3916d
Yaadleen Sihota <92286202+YaadSiho@users.noreply.github.com>
2022-10-12 10:16:12 +1000

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,13 @@
+var modalEle = document.querySelector(".modal");
+var modalImage = document.querySelector(".modalImage");
+var captionText = document.getElementById("#caption");
+Array.from(document.querySelectorAll(".ImgThumbnail")).forEach(item => {
+   item.addEventListener("click", event => {
+      modalEle.style.display = "block";
+      modalImage.src = event.target.src;
+      captionText.innerHTML = this.alt;
+   });
+});
+document.querySelector(".close").addEventListener("click", () => {
+   modalEle.style.display = "none";
+});
